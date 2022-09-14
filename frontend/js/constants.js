4931a371b37fe6977680aac21a920132229433db
Browsing2022 <bulldog1063@hotmail.com>
2022-09-14 13:31:41 +0100

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x320Cd56ed5C55aAC76943B90B505A5a39019e690"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the first Mintingames NFT Project... Noughts and Crosses!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'Noughts and Crosses is the genesis nft collection for mintingames, as such we will be giving away 400 matic every 100 mints until 1000 and then 4000 matic every 1000 mints up to mint out at 5000';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
