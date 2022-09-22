3d758749bd82f6f03dd7edea673052c0dd08ad72
Browsing2022 <bulldog1063@hotmail.com>
2022-09-22 19:31:23 +0100

    changes

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,8 +1,8 @@
 const contractAddress = "0x320Cd56ed5C55aAC76943B90B505A5a39019e690"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welcome to Noughts and Crosses! the first mint from mintingames!";
-const welcome_h2 = "400Matic given away every 100 mints!";
+const welcome_h1 = "400 Matic given away every 100 mints!";
+const welcome_h2 = "Welcome to Noughts and Crosses! the first mint from mintingames!";
 const welcome_p = 'mint your board now!';
 const h1_presale_coming_soon = "NFT Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
